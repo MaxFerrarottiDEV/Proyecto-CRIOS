b5e5529d0fcfe0ea7c66b2a93d45c9a9e977a317
MaxFerrarottiDEV <maximo_ferrarotti@hotmail.com>
2024-09-03 20:36:34 -0300

    Modulo de registro, inicio y cierre de sesion funcionando

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\crios-proyecto\FundacionCRIOS\env
+set VIRTUAL_ENV=C:\Users\Estudiante\Downloads\isdm\2do Cuatrimestre\Taller de Programacion\TestLoginRegisterLogoutMain\env
 
 if not defined PROMPT set PROMPT=$P$G
 
