a1f325ebf01477225af17e7159d3d9b89764d7cb
MaxFerrarottiDEV <maximo_ferrarotti@hotmail.com>
2024-09-26 05:20:12 -0300

    Implementacion de una carpeta para archivos css, cambios realizas en .gitignore, agregada la base de datos del proyecto y actualizacion del template hometest

--- MiApp/urls.py
+++ MiApp/urls.py
@@ -1,6 +1,6 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 
 urlpatterns = [
