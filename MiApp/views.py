d9e87216561f2af6aff35cac254a4ea332e6f3f2
MaxFerrarottiDEV <maximo_ferrarotti@hotmail.com>
2024-11-08 15:44:19 -0300

    union de todas las ramas

--- MiApp/views.py
+++ MiApp/views.py
@@ -1,33 +1,42 @@
-from django.shortcuts import render, redirect, get_object_or_404 # type: ignore
-from django.contrib.auth.decorators import login_required # type: ignore
-from .forms import PreinscripcionForm , CustomRegisterForm
-from django.contrib import messages # type: ignore
-from .models import DatInsc, Estudiantes
-from datetime import date
+from django.shortcuts import render, redirect ,get_object_or_404 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import PreinscripcionForm 
+from django.http import JsonResponse
+from .models import DatInsc, EstadosCurriculares ,Estudiantes,Materias,MateriasxplanesEstudios,PlanesEstudios
 
+from datetime import date
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+from io import BytesIO
+from reportlab.lib.pagesizes import letter # type: ignore
+from reportlab.pdfgen import canvas # type: ignore
+from reportlab.lib import colors # type: ignore
+from reportlab.lib.units import inch # type: ignore
+from reportlab.platypus import Table, TableStyle # type: ignore
 
 def login(request):
     return render(request, 'login.html')
 
-def register_view(request):
+def register(request):
     if request.method == 'POST':
-        form = CustomRegisterForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()  # Esto guarda el usuario en la base de datos
-            messages.success(request, 'Tu cuenta ha sido creada con éxito. ¡Ahora puedes iniciar sesión!')
-            return redirect('login')  # Redirigir a la página de inicio de sesión
+            form.save() 
+            messages.success(request, '¡Cuenta creada exitosamente! Ahora puedes iniciar sesión.')
+            return redirect('login')  
         else:
-            messages.error(request, 'Hubo un error en la creación de la cuenta. Inténtalo de nuevo.')
+            messages.error(request, 'Hubo un error en la creación de la solicitud. Inténtalo de nuevo.')
     else:
-        form = CustomRegisterForm()
-    return render(request, 'registration/register.html', {'form': form})
+        form = UserCreationForm()
+    
+    return render(request, 'registration/register.html', {'form': form}) 
 
-def test(request):
-    return render(request, 'test.html')
 
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    return render(request,'home.html')
 
 @login_required
 def tipo_inscripcion(request):
@@ -35,8 +44,8 @@ def tipo_inscripcion(request):
 
 @login_required
 def lista_solicitudes(request):
-    # Filtramos las solicitudes que no están confirmadas (cuando ambos campos son False)
-    solicitudes = DatInsc.objects.filter(matricula=False, legajo_fisico=False)
+    
+    solicitudes = DatInsc.objects.filter
 
     # Inicializamos el formulario de la solicitud
     form = PreinscripcionForm()
@@ -89,11 +98,8 @@ def confirmar_solicitud(request, id_datinsc):
 
 @login_required
 def editar_solicitud(request, id_datinsc):
-    # Obtener la solicitud por su ID
     solicitud = get_object_or_404(DatInsc, id_datinsc=id_datinsc)
-
     if request.method == 'POST':
-        # Llenar el formulario con los datos POST
         form = PreinscripcionForm(request.POST, instance=solicitud)
         if form.is_valid():
             form.save()
@@ -102,87 +108,199 @@ def editar_solicitud(request, id_datinsc):
         else:
             messages.error(request, 'Hubo un error en el formulario. Por favor, corrige los errores.')
     else:
-        # Pre-poblar el formulario con los datos de la solicitud
         form = PreinscripcionForm(instance=solicitud)
 
     return render(request, 'inscripciones/solicitudes/editar_solicitud.html', {'form': form})
 
 @login_required
 def eliminar_solicitud(request, id_datinsc):
-    # Obtener la solicitud o devolver un 404 si no existe
     solicitud = get_object_or_404(DatInsc, id_datinsc=id_datinsc)
     
     if request.method == "POST":
         try:
             solicitud.delete()
-            # Si la eliminación es exitosa, enviamos un mensaje de éxito
             messages.success(request, "La solicitud ha sido eliminada correctamente.")
         except Exception as e:
-            # Si ocurre un error, enviamos un mensaje de error
             messages.error(request, f"Ocurrió un error al eliminar la solicitud: {str(e)}")
-        
-        # Redirigir a la lista de solicitudes
         return redirect('lista_solicitudes')
-    
-    # Si no es POST, simplemente renderizar la página de confirmación
     return render(request, 'inscripciones/solicitudes/eliminar_solicitud.html', {'solicitud': solicitud})
+
 
 @login_required
 def consultas(request):
     dni = request.GET.get('dni')  
     if dni:
-        estudiante = DatInsc.objects.filter(dni=dni).first()
-        if estudiante:
-            print(estudiante.id_datinsc)  
+        estudiante_datinsc = DatInsc.objects.filter(dni=dni).first()
+        if estudiante_datinsc:
+            estudiante = Estudiantes.objects.filter(id_datinsc=estudiante_datinsc).first()
             context = {'estudiante': estudiante}
         else:
-
             context = {'error': 'Estudiante no encontrado'}
     else:
-        
-        lista_estudiantes = DatInsc.objects.all()
-        for est in lista_estudiantes:
-            print(est.id_datinsc) 
+        lista_estudiantes = Estudiantes.objects.select_related('id_datinsc').all()
         context = {'estudiantes': lista_estudiantes}
     
-    return render(request, 'inscripciones/consultas/consultas.html',context)
+    return render(request, 'inscripciones/consultas/consultas.html', context)
+
 
 @login_required
-def modificar(request, id):
-    estudiante = get_object_or_404(DatInsc, pk=id)
+def modificar(request, dni):
+    estudiante = get_object_or_404(DatInsc, dni=dni)
+    
     if request.method == 'POST':
-        # Procesar los datos del formulario de modificación (añade tu lógica de actualización aquí)
         estudiante.nombre = request.POST.get('nombre')
         estudiante.apellido = request.POST.get('apellido')
         estudiante.dni = request.POST.get('dni')
-        estudiante.Celular_Nro = request.POST.get('telefono')
+        estudiante.celular_nro = request.POST.get('telefono')
         estudiante.email = request.POST.get('email')
         estudiante.domicilio = request.POST.get('domicilio')
+        estudiante.matricula = request.POST.get('matricula') == 'on'
+        estudiante.legajo_fisico = request.POST.get('legajo_fisico') == 'on'
         estudiante.save()
         messages.success(request, '¡Estudiante modificado exitosamente!')
         return redirect('consultas')
     return render(request, 'inscripciones/consultas/modificar.html', {'estudiante': estudiante})
 
 
-# Vista para eliminar estudiante
 @login_required
-def eliminar_estudiante_ajax(request):
-    estudiante_id = request.POST.get('id')
-    estudiante = get_object_or_404(DatInsc, id_datinsc=estudiante_id)
+def eliminar_estudiante(request, dni):
+    estudiante = get_object_or_404(Estudiantes, id_datinsc__dni=dni)
     estudiante.delete()
-    return JsonResponse({'success': True, 'message': 'Estudiante eliminado'}) # type: ignore
+    messages.success(request, 'El estudiante ha sido eliminado exitosamente.')
+    return redirect('consultas')
 
-
-# Vista para adjuntar un archivo
 @login_required
-def adjuntar_archivo(request):
-    if request.method == 'POST':
-        try:
-            archivo = request.FILES['fileUpload']
-            return HttpResponse('Archivo subido exitosamente.') # type: ignore
-        except KeyError:
-            return HttpResponse('No se encontró el archivo.') # type: ignore
-    return render(request, 'Insc/consultas.html')
-
 def build(request):
     return render(request, 'build.html')
+
+@login_required
+def plan_estudio_view(request):
+    planes = PlanesEstudios.objects.all()
+    return render(request, 'estadosCurriculares/planesEstudios/planestudio.html', {'planes': planes})
+
+@login_required
+def verEstado(request, dni): 
+    estudiante = get_object_or_404(Estudiantes, id_datinsc__dni=dni)
+    estado_curricular = EstadosCurriculares.objects.filter(id_estudiante_estcur=estudiante) 
+    return render(request, 'estadosCurriculares/verEstado.html', {
+        'estudiante': estudiante,
+        'estado_curricular': estado_curricular})
+
+@login_required
+def estados(request):
+    dni = request.GET.get('dni')
+    if dni:
+        estudiante_datinsc = DatInsc.objects.filter(dni=dni).first()
+        if estudiante_datinsc:
+            estudiante = Estudiantes.objects.filter(id_datinsc=estudiante_datinsc).first()
+            if estudiante:
+                estado_curricular = EstadosCurriculares.objects.filter(id_estudiante_estcur=estudiante)
+                materias = Materias.objects.all()
+                context = {
+                    'estudiante': estudiante,
+                    'estudiante_datinsc': estudiante_datinsc,
+                    'estado_curricular': estado_curricular,
+                    'materias': materias  }
+            else:
+                context = {'error': 'No se encontró el estado curricular para este estudiante.'}
+        else:
+            context = {'error': 'Estudiante no encontrado.'}
+    else:
+        lista_estudiantes = Estudiantes.objects.all()
+        materias = Materias.objects.all()  #
+        context = {
+            'estudiantes': lista_estudiantes,
+            'materias': materias }
+
+    return render(request, 'estadosCurriculares/estados.html', context)
+
+@login_required
+def agregarNota(request):
+    if request.method == 'POST':
+        materia_id = request.POST.get('materia')
+        condicion_nota = request.POST.get('condicion')
+        nota = request.POST.get('nota')
+        fecha_finalizacion = request.POST.get('fecha')
+        estudiante_id = request.POST.get('estudiante_id')
+
+        nueva_nota = EstadosCurriculares(
+            id_matxplan_estcur_id=materia_id, 
+            id_estudiante_estcur_id=estudiante_id,
+            condicion_nota=condicion_nota,
+            nota=nota,
+            fecha_finalizacion=fecha_finalizacion
+        )
+        nueva_nota.save()
+        return redirect('estados')
+    else:
+        materias = Materias.objects.all()
+        return render(request, 'estados.html', {'materias': materias})
+
+@login_required    
+def agregar_nota(request, dni):
+    estudiante = get_object_or_404(Estudiantes, id_datinsc__dni=dni)
+    
+    if request.method == 'POST':
+        materia_id = request.POST.get('materia')
+        condicion = request.POST.get('condicion')
+        nota = request.POST.get('nota')
+        fecha = request.POST.get('fecha')
+
+        # Crear la nueva nota
+        materia = get_object_or_404(Materias, id=materia_id)
+        nuevo_estado = EstadosCurriculares(
+            id_estudiante=estudiante,
+            id_matxplan=materia,
+            condicion_nota=condicion,
+            nota=nota,
+            fecha_finalizacion=fecha
+        )
+        nuevo_estado.save()
+        return redirect('estado_curricular', dni=dni)
+    materias = Materias.objects.all()
+    return render(request, 'estadosCurriculares/agregar_nota.html', {'estudiante': estudiante, 'materias': materias})
+
+@login_required
+def pdf_estadoCurricular(request):
+    dni = request.GET.get('dni')
+    if not dni:
+        return HttpResponse("DNI no proporcionado.", status=400)
+    estudiante = get_object_or_404(Estudiantes, id_datinsc__dni=dni)
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer, pagesize=letter)
+    p.drawString(100, 710, f"Apellidos: {estudiante.id_datinsc.apellido}"),p.drawString(100, 690, f"Nombres: {estudiante.id_datinsc.nombre}")
+    p.drawString(100, 750, f"Legajo N: {estudiante.nro_legajo}"), p.drawString(100, 730, f"DNI: {estudiante.id_datinsc.dni}")
+    p.drawString(100, 650, "Notas del Estudiante")
+    estado_curricular = estudiante.estadoscurriculares_set.all()
+   
+    data = [["Materia", "Estado", "Nota", "Fecha"]]  
+    for materia in estado_curricular:
+        data.append([
+            materia.id_matxplan_estcur.id_materia.nombre,
+            materia.condicion_nota,
+            str(materia.nota),
+            materia.fecha_finalizacion.strftime("%d/%m/%Y")
+        ])
+    
+    table = Table(data, colWidths=[2*inch, 1.5*inch, 1*inch, 1.5*inch])
+    table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),  
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 10),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ]))
+
+    table.wrapOn(p, 100, 500)
+    table.drawOn(p, 100, 500)
+
+    p.showPage()
+    p.save()
+    
+    buffer.seek(0)
+    response = HttpResponse(buffer, content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{estudiante.id_datinsc.apellido} {estudiante.id_datinsc.nombre}-Estado Curricular.pdf"'
+    return response
