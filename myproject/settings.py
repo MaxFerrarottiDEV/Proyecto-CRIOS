092c72aa8f1fb569777a8fc45aa3dd3dc2d65531
juanamaya8976 <juan244cja@gmail.com>
2024-11-26 16:17:06 -0300

    se agrego carpeta firebase con cambios en settings.py y .gitignore

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-sbi45=(i6ddz!v@7$cq1pb#7-0znfk%)v3o56_5xvgxsdy=6%9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,7 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+STATICFILES_DIRS = [
+    STATIC_DIR,
+    BASE_DIR / 'static',
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
